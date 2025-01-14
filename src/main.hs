61dc8fcd1c74849cc450eba82b12df95f423613c
Taylor <taylorpat@umsystem.edu>
2025-01-14 03:02:27 -0600

    linting tips

--- src/main.hs
+++ src/main.hs
@@ -17,4 +17,4 @@ main = do
   -- This is an example of applying one function (head exampleGenome produces intAdd) to the exampleState:
   assert ([3, 3] == map unpackIntGene (int (head exampleGenome exampleState))) pure ()
   -- This function applies an entire genome to the starting state, and produces the final state:
-  assert ([6] == (map unpackIntGene (int (interpretGenome exampleState exampleGenome)))) pure ()
+  assert ([6] == map unpackIntGene (int (interpretGenome exampleState exampleGenome))) pure ()
