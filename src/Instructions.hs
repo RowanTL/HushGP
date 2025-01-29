53d62f31c7057ee039784a81084d345fb8046681
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-01-29 14:50:42 -0600

    add instructions to re export

---
+++ src/Instructions.hs
@@ -0,0 +1,24 @@
+module Instructions (
+  module Instructions.GenericInstructions,
+  module Instructions.IntInstructions,  
+  module Instructions.FloatInstructions,
+  module Instructions.StringInstructions,
+  module Instructions.CharInstructions,
+  module Instructions.CodeInstructions,
+  module Instructions.ExecInstructions,
+  module Instructions.LogicalInstructions,
+  module Instructions.VectorIntInstructions,
+  module Instructions.VectorFloatInstructions
+)
+where
+
+import Instructions.GenericInstructions
+import Instructions.IntInstructions
+import Instructions.FloatInstructions
+import Instructions.StringInstructions
+import Instructions.CharInstructions
+import Instructions.CodeInstructions
+import Instructions.ExecInstructions
+import Instructions.LogicalInstructions
+import Instructions.VectorIntInstructions
+import Instructions.VectorFloatInstructions
