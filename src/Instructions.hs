7ea3fe6d5ce22c1b9e7bff3b00e961ec930697af
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-02-02 14:56:53 -0600

    large state test

--- src/Instructions.hs
+++ src/Instructions.hs
@@ -12,6 +12,7 @@ module Instructions
     module Instructions.VectorStringInstructions,
     module Instructions.VectorLogicalInstructions,
     module Instructions.VectorCharInstructions,
+    largeState,
   )
 where
 
@@ -28,3 +29,26 @@ import Instructions.VectorFloatInstructions
 import Instructions.VectorIntInstructions
 import Instructions.VectorLogicalInstructions
 import Instructions.VectorStringInstructions
+import State
+import Data.Map qualified as Map
+
+largeState :: State
+largeState =
+  State
+    { _exec = replicate 100000 $ StateFunc (instructionIntAdd, "instructionIntAdd"),
+      _code = replicate 100 $ StateFunc (instructionIntAdd, "instructionIntAdd"),
+      _int = [1..100200],
+      _float = [1.0..120.00],
+      _bool = replicate 100 True,
+      _string = replicate 100 "",
+      _char = replicate 100 'z',
+      _parameter = [],
+      _vectorInt = replicate 100 [1,2,3],
+      _vectorFloat = replicate 100 [1.0, 2.0, 3.0],
+      _vectorBool = replicate 100 [True, False],
+      _vectorString = replicate 100 ["hello", "there"],
+      _vectorChar = replicate 100 ['a','b','c'],
+      _input = Map.empty
+    }
+
+
