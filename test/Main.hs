24398989be82959a6c9de38203ef3d482027f435
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-02-13 00:38:21 -0600

    make some tests runnable

--- test/Main.hs
+++ test/Main.hs
@@ -1,7 +1,7 @@
-import Instructions
-import Push
-import PushTests
-import State
+-- import HushGP.Instructions
+-- import HushGP.Push
+import HushGP.PushTests
+-- import HushGP.State
 import Test.QuickCheck
 
 -- import Data.List
@@ -19,3 +19,23 @@ qcw = quickCheckWith pushTestArgs
 
 vcw :: (Testable a) => a -> IO ()
 vcw = verboseCheckWith pushTestArgs
+
+main :: IO ()
+main = do
+  qcw prop_IntAdd
+  qcw prop_IntSub
+  qcw prop_IntMul
+  qcw prop_IntDiv
+  qcw prop_IntMod
+  qcw prop_IntFromFloat
+  qcw prop_IntFromBool
+  qcw prop_IntMin
+  qcw prop_IntMax
+  qcw prop_IntInc
+  qcw prop_IntDec
+  qcw prop_IntLT
+  qcw prop_IntGT
+  qcw prop_IntLTE
+  qcw prop_IntGTE
+  qcw prop_IntDup
+  qcw prop_IntPop
