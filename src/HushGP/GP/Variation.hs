747bf57d03270514cd6028653edf97a07b0d6421
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-02-25 14:59:23 -0600

    add variation file

---
+++ src/HushGP/GP/Variation.hs
@@ -0,0 +1,7 @@
+module HushGP.GP.Variation where
+
+import HushGP.Genome
+import HushGP.GP.PushArgs
+
+newIndividual :: PushArgs -> [Individual] -> Individual
+newIndividual = error "Implement this later"
