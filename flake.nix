53c490b3b3450c51fa706c8f20b6584b47ae5d27
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-02-25 15:23:01 -0600

    mission failed

--- flake.nix
+++ flake.nix
@@ -56,10 +56,6 @@
 
             # Check that haskell-language-server works
             # hlsCheck.enable = true; # Requires sandbox to be disabled
-
-            shellHook = ''
-            echo "Entering HushGP environment"
-            '';
           };
         };
 
