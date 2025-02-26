2f6675e9f5ebb5421ee841754d746d5cf7b555d6
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-02-25 18:47:50 -0600

    add downsample

---
+++ src/HushGP/GP/Downsample.hs
@@ -0,0 +1,7 @@
+module HushGP.GP.Downsample where
+
+import HushGP.State
+import HushGP.Genome
+
+updateCaseDistances :: [Individual] -> ([[Gene]], [Gene]) -> ([[Gene]], [Gene]) -> String -> Double -> ([[Gene]], [Gene])
+updateCaseDistances evaledPop downsampleData trainingData informedDownsamplingType solutionThreshold = undefined
